41beaa7a76fd67e8c6eaf437b9481e2eac5d8d1c
Tanya Sandoval <tanya.sandoval@kochind.com>
2017-02-14 10:26:15 +0000

    Fixed bbh function to return dataframe with the columns in the same order as requested in the input field list. Also fixed some formatting of white space

--- pybbg/pybbg_k.py
+++ pybbg/pybbg_k.py
@@ -30,13 +30,13 @@ class Pybbg():
 
         # Create a Session
         self.session = blpapi.Session(sessionOptions)
-    
+
         # Start a Session
         if not self.session.start():
             print("Failed to start session.")
-        
+
         self.session.nextEvent()
-        
+
     def service_refData(self):
         """
         init service for refData
@@ -44,20 +44,19 @@ class Pybbg():
         if '//blp/refdata' in self.initialized_services:
             return
 
-
         if not self.session.openService("//blp/refdata"):
             print("Failed to open //blp/refdata")
-        
+
         self.session.nextEvent()
-        
+
         # Obtain previously opened service
         self.refDataService = self.session.getService("//blp/refdata")
-        
-        self.session.nextEvent() 
 
-        self.initialized_services.add('//blp/refdata') 
-    
-    def bdh(self, ticker_list, fld_list, start_date, end_date=date.today().strftime('%Y%m%d'), periodselection = 'DAILY'):
+        self.session.nextEvent()
+
+        self.initialized_services.add('//blp/refdata')
+
+    def bdh(self, ticker_list, fld_list, start_date, end_date=date.today().strftime('%Y%m%d'), periodselection='DAILY'):
         """
         Get ticker_list and field_list
         return pandas multi level columns dataframe
@@ -66,9 +65,9 @@ class Pybbg():
         self.service_refData()
 
         if isstring(ticker_list):
-            ticker_list = [ ticker_list ]
+            ticker_list = [ticker_list]
         if isstring(fld_list):
-            fld_list = [ fld_list ] 
+            fld_list = [fld_list]
 
         if hasattr(start_date, 'strftime'):
             start_date = start_date.strftime('%Y%m%d')
@@ -77,38 +76,41 @@ class Pybbg():
 
         request = self.refDataService.createRequest("HistoricalDataRequest")
         for t in ticker_list:
-            request.getElement("securities").appendValue(t)       
-        for f in fld_list :
+            request.getElement("securities").appendValue(t)
+        for f in fld_list:
             request.getElement("fields").appendValue(f)
         request.set("periodicityAdjustment", "ACTUAL")
         request.set("periodicitySelection", periodselection)
         request.set("startDate", start_date)
         request.set("endDate", end_date)
-        
-        #print("Sending Request:", request)
+
+        # print("Sending Request:", request)
         # Send the request
         self.session.sendRequest(request)
         # defaultdict - later convert to pandas
         data = defaultdict(dict)
         # Process received events
-        while(True):
+        while (True):
             # We provide timeout to give the chance for Ctrl+C handling:
             ev = self.session.nextEvent(500)
             for msg in ev:
                 ticker = msg.getElement('securityData').getElement('security').getValue()
                 fieldData = msg.getElement('securityData').getElement('fieldData')
-                for i in range(fieldData.numValues()) :
-                    for j in range(1,fieldData.getValue(i).numElements()) :
-                        data[(ticker, fld_list[j-1])][fieldData.getValue(i).getElement(0).getValue()] = fieldData.getValue(i).getElement(j).getValue()
-        
+                for i in range(fieldData.numValues()):
+                    for j in range(1, fieldData.getValue(i).numElements()):
+                        data[(ticker, fld_list[j - 1])][
+                            fieldData.getValue(i).getElement(0).getValue()] = fieldData.getValue(i).getElement(
+                            j).getValue()
+
             if ev.eventType() == blpapi.Event.RESPONSE:
                 # Response completly received, so we could exit
                 break
 
         if len(fld_list) == 1:
-            data = { k[0] : v for k,v in data.items() }
+            data = {k[0]: v for k, v in data.items()}
             data = DataFrame(data)
-            #data.index = pd.to_datetime(data.index)
+            data = data[ticker_list]
+            data.index = pd.to_datetime(data.index)
             return data
 
         if len(data) == 0:
@@ -116,11 +118,12 @@ class Pybbg():
             return DataFrame()
 
         data = DataFrame(data)
+        data = data[ticker_list]
         data.columns = pd.MultiIndex.from_tuples(data, names=['ticker', 'field'])
         data.index = pd.to_datetime(data.index)
         return data
 
-    def bdib(self, ticker, fld_list, startDateTime, endDateTime, eventType='TRADE', interval = 1):
+    def bdib(self, ticker, fld_list, startDateTime, endDateTime, eventType='TRADE', interval=1):
         """
         Get one ticker (Only one ticker available per call); eventType (TRADE, BID, ASK,..etc); interval (in minutes)
                 ; fld_list (Only [open, high, low, close, volumne, numEvents] availalbe)
@@ -131,25 +134,26 @@ class Pybbg():
         request = self.refDataService.createRequest("IntradayBarRequest")
         request.set("security", ticker)
         request.set("eventType", eventType)
-        request.set("interval", interval)  # bar interval in minutes        
+        request.set("interval", interval)  # bar interval in minutes
         request.set("startDateTime", startDateTime)
         request.set("endDateTime", endDateTime)
-        
-        #print "Sending Request:", request
+
+        # print "Sending Request:", request
         # Send the request
         self.session.sendRequest(request)
         # defaultdict - later convert to pandas
         data = defaultdict(dict)
         # Process received events
-        while(True):
+        while (True):
             # We provide timeout to give the chance for Ctrl+C handling:
             ev = self.session.nextEvent(500)
             for msg in ev:
                 barTickData = msg.getElement('barData').getElement('barTickData')
-                for i in range(barTickData.numValues()) :
-                    for j in range(len(fld_list)) :
-                        data[(fld_list[j])][barTickData.getValue(i).getElement(0).getValue()] = barTickData.getValue(i).getElement(fld_list[j]).getValue()
-        
+                for i in range(barTickData.numValues()):
+                    for j in range(len(fld_list)):
+                        data[(fld_list[j])][barTickData.getValue(i).getElement(0).getValue()] = barTickData.getValue(
+                            i).getElement(fld_list[j]).getValue()
+
             if ev.eventType() == blpapi.Event.RESPONSE:
                 # Response completly received, so we could exit
                 break
@@ -157,38 +161,36 @@ class Pybbg():
         data.index = pd.to_datetime(data.index)
         return data
 
-
     def bdp(self, ticker, fld_list, overrides=None):
         # print(ticker, fld_list, overrides)
         self.service_refData()
-        
+
         request = self.refDataService.createRequest("ReferenceDataRequest")
         if isstring(ticker):
-            ticker = [ ticker ]
+            ticker = [ticker]
 
         securities = request.getElement("securities")
         for t in ticker:
             securities.appendValue(t)
-        
+
         if isstring(fld_list):
-            fld_list = [ fld_list ]
+            fld_list = [fld_list]
 
         fields = request.getElement("fields")
         for f in fld_list:
             fields.appendValue(f)
-        
+
         if overrides is not None:
             overrideOuter = request.getElement('overrides')
             for k in overrides:
                 override1 = overrideOuter.appendElement()
-                override1.setElement('fieldId', k) 
+                override1.setElement('fieldId', k)
                 override1.setElement('value', overrides[k])
-
 
         self.session.sendRequest(request)
         data = dict()
 
-        while(True):
+        while (True):
             # We provide timeout to give the chance for Ctrl+C handling:
             ev = self.session.nextEvent(500)
             for msg in ev:
@@ -203,20 +205,17 @@ class Pybbg():
                             data[secId][field] = fieldData.getElement(field).getValue()
                         else:
                             data[secId][field] = np.NaN
-                        
 
-        
             if ev.eventType() == blpapi.Event.RESPONSE:
                 # Response completly received, so we could exit
                 break
 
         return pd.DataFrame.from_dict(data)
 
-
     # def bdp(self, ticker, fld_list):
 
     #     self.service_refData()
-        
+
     #     request = self.refDataService.createRequest("ReferenceDataRequest")
     #     if isstring(ticker):
     #         ticker = [ ticker ]
@@ -224,14 +223,14 @@ class Pybbg():
     #     securities = request.getElement("securities")
     #     for t in ticker:
     #         securities.appendValue(t)
-        
+
     #     if isstring(fld_list):
     #         fld_list = [ fld_list ]
 
     #     fields = request.getElement("fields")
     #     for f in fld_list:
     #         fields.appendValue(f)
-        
+
 
     #     self.session.sendRequest(request)
     #     data = dict()
@@ -251,9 +250,9 @@ class Pybbg():
     #                         data[secId][field] = fieldData.getElement(field).getValue()
     #                     else:
     #                         data[secId][field] = np.NaN
-                        
 
-        
+
+
     #         if ev.eventType() == blpapi.Event.RESPONSE:
     #             # Response completly received, so we could exit
     #             break
@@ -261,18 +260,17 @@ class Pybbg():
     #     return pd.DataFrame.from_dict(data)
 
 
-    def bds(self, security, field, overrides = None):
+    def bds(self, security, field, overrides=None):
 
         self.service_refData()
-        
+
         request = self.refDataService.createRequest("ReferenceDataRequest")
         assert isstring(security)
         assert isstring(field)
 
-
         securities = request.getElement("securities")
         securities.appendValue(security)
-        
+
         fields = request.getElement("fields")
         fields.appendValue(field)
 
@@ -280,15 +278,14 @@ class Pybbg():
             overrideOuter = request.getElement('overrides')
             for k in overrides:
                 override1 = overrideOuter.appendElement()
-                override1.setElement('fieldId', k) 
+                override1.setElement('fieldId', k)
                 override1.setElement('value', overrides[k])
 
-        
         # print(request)
         self.session.sendRequest(request)
         data = dict()
 
-        while(True):
+        while (True):
             # We provide timeout to give the chance for Ctrl+C handling:
             ev = self.session.nextEvent(500)
             for msg in ev:
@@ -306,15 +303,15 @@ class Pybbg():
 
                             data[k].append(v)
 
-        
             if ev.eventType() == blpapi.Event.RESPONSE:
                 # Response completly received, so we could exit
                 break
 
         return pd.DataFrame.from_dict(data)
-        
+
     def stop(self):
         self.session.stop()
+
 
 def isstring(s):
     # if we use Python 3
@@ -322,7 +319,6 @@ def isstring(s):
         return isinstance(s, str)
     # we use Python 2
     return isinstance(s, basestring)
-
 
 
 def processMessage(msg):
