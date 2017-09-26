9df9d803baa679bfaaf221197cd9cfd6fcbc2e37
Wayne Nilsen <wnilsen@frontlightcapital.com>
2017-09-26 10:04:21 -0400

    additional request parameters and date adjustments for bdh

--- test_pybbg.py
+++ test_pybbg.py
@@ -1,6 +1,7 @@
 import unittest
 import pybbg
 import datetime 
+from dateutil.relativedelta import relativedelta
 
 class TestPybbg(unittest.TestCase):
     def test_bdp(self):
@@ -21,7 +22,7 @@ class TestPybbg(unittest.TestCase):
     def test_bds_override(self):
         tester = pybbg.Pybbg()
         data = tester.bds('EDA Comdty', 'FUT_CHAIN_LAST_TRADE_DATES', overrides={'INCLUDE_EXPIRED_CONTRACTS': 'Y'})
-        print(data) 
+        print(data)
 
     def test_bdh(self):
         tester = pybbg.Pybbg()
@@ -48,6 +49,47 @@ class TestPybbg(unittest.TestCase):
         tester = pybbg.Pybbg()
         data = tester.bdh('260555 Equity', ['PX_LAST','PX_BID','PX_ASK'], datetime.datetime.today() + datetime.timedelta(days=-10), datetime.datetime.today())
         print(data)
+
+    def test_bdh_mixed_dates_monthly(self):
+        tester = pybbg.Pybbg()
+        data = tester.bdh(
+            ['FEDL01 Index', 'USSWAP10 Curncy'],
+            'PX_LAST', datetime.date.today() - relativedelta(years=2),
+            datetime.date.today(),
+            'MONTHLY',
+            overrides=dict(
+                CALENDAR_CONVENTION=1 # calendar convention 'calendar' flds rk408
+            ),
+            other_request_parameters=dict(
+                periodicityAdjustment='CALENDAR',
+                nonTradingDayFillMethod='PREVIOUS_VALUE',
+                returnRelativeDate=True
+            ),
+            move_dates_to_period_end=True
+        ).iloc[::-1]
+
+        print(data)
+
+    def test_bdh_mixed_dates_weekly(self):
+        tester = pybbg.Pybbg()
+        data = tester.bdh(
+            ['EONIA Index', 'USSWAP10 Curncy'],
+            'PX_LAST', datetime.date.today() - relativedelta(years=2),
+            datetime.date.today(),
+            'WEEKLY',
+            overrides=dict(
+                CALENDAR_CONVENTION=1 # calendar convention 'calendar' flds rk408
+            ),
+            other_request_parameters=dict(
+                periodicityAdjustment='CALENDAR',
+                nonTradingDayFillMethod='PREVIOUS_VALUE',
+                returnRelativeDate=True
+            ),
+            move_dates_to_period_end=True
+        ).iloc[::-1]
+        print(data)
+
+
 
 if __name__ == '__main__':
     unittest.main()
